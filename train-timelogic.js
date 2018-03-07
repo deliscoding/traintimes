3faf8e6ae7e13beb2eea06e3b1ab494be3f592df
detour35 <danelis19@gmail.com>
2018-03-07 08:44:24 -0500

    added btn to js file and worked on train logic

--- train-timelogic.js
+++ train-timelogic.js
@@ -19,3 +19,44 @@
   };
   firebase.initializeApp(config);
 var trainData = firebase.database();
+
+// Adding Trains Button 
+$("#add-train-btn").on("click", function() {
+
+  // The variables below get user input
+  var trainName = $("#train-name-input").val().trim();
+  var destination = $("#destination-input").val().trim();
+  var firstTrain = $("#first-train-input").val().trim();
+  var frequency = $("#frequency-input").val().trim();
+
+  // Creates local "temporary" object for holding train's data
+  var newTrain = {
+
+    name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  };
+
+  // Uploads train data to the database
+  // trainData refers to the var above-- firebase.database();
+  trainData.ref().push(newTrain);
+
+  // Logs all of the train info to the console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
+
+  // Alert that Train was added
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+
+  // Determine when the next train arrives.
+  return false;
+});
